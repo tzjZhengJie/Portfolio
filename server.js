4fd70500e6a320d919558669c881ca72bbfd1766
tzjZhengJie <zhengjie129@gmail.com>
2024-09-29 15:57:07 +0800

    Add new files and configurations

---
+++ server.js
@@ -0,0 +1,25 @@
+// Import required modules
+const express = require('express');
+const path = require('path');
+
+// Create an Express application
+const app = express();
+
+// Set the port for the server
+const PORT = process.env.PORT || 3000;
+
+// Middleware to serve static files from the 'assets' directory
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+// Middleware to serve static files from the 'images' directory
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Route to serve the main HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
